c3a33d1a67a2489ccb7d64cd74f05a9be52cf056
Matthew Feng <mattfeng@mit.edu>
2019-12-05 06:04:22 +0000

    Upload player complete

--- scripts/upload_player.py
+++ scripts/upload_player.py
@@ -17,11 +17,15 @@ import requests
 import getpass
 import boto3
 import uuid
+import json
+import os
 
+from botocore.exceptions import ClientError
 from pyleiser.util import error, info
 
 # Location of the player repo server
-PLAYER_REPO_SERVER = 'http://34.233.102.237/add'
+PLAYER_REPO_SERVER = 'http://34.233.102.237/'
+CUSTOM_AWS_CREDS = json.load(open('creds.json'))
 CUSTOM_S3_BUCKET = 'leiserchess'
 
 # Amazon S3 bucket name.
@@ -42,8 +46,24 @@ def add_player_to_db(name, official_name, desc):
         'official_name': official_name,
         'desc': desc
     }
-    resp = requests.post(PLAYER_REPO_SERVER, data = data)
-    print(resp.text)
+    resp = requests.post(PLAYER_REPO_SERVER + '/add', data = data)
+    resp_json = json.loads(resp.text)
+    if 'error' in resp_json:
+        err = resp_json['error']
+        error('Failed to add player `{}` to our custom database: {}'.format(name, err))
+        exit(1)
+    else:
+        info(resp.text)
+
+def check_player_in_db(name):
+    resp = requests.post(PLAYER_REPO_SERVER + '/check', data = {'name': name})
+    if resp.text == 'exists':
+        error('A bot named `{}` already exists!'.format(name))
+        exit(1)
+    elif resp.text == 'bad request':
+        error('A malformed request was made')
+        exit(1)
+
 
 def get_user():
     try:
@@ -134,15 +154,46 @@ def zip_source_code(srcpath):
     shutil.make_archive(fname, 'zip', srcpath)
     return fname + '.zip'
 
-def upload_binary_to_custom_s3_bucket():
+def upload_binary_to_custom_s3_bucket(botname, filepath, zipfile, bucket):
     ''' Uploads a binary and source directory to a custom S3 bucket. '''
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client('s3',
+        **CUSTOM_AWS_CREDS
+    )
+
+    binary_obj = botname
+    if zipfile is not None:
+        zip_obj = '{}-src.zip'.format(botname)
+
+    try:
+        s3_client.upload_file(filepath, bucket, binary_obj)
+        if zipfile is not None:
+            s3_client.upload_file(zipfile, bucket, zip_obj)
+    except ClientError as e:
+        error('Uploading binary and source to custom S3 failed.')
+        print(e)
+        exit(1)
+    
+    if zipfile is not None:
+        info('Uploaded source `{}` and binary `{}` successfully!'.format(zip_obj, binary_obj))
+    else:
+        info('Uploaded binary `{}` successfully!'.format(binary_obj))
+
 
 def main(binary, srcpath, botname, botdesc):
     ALLOWED_CHARS = set('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_')
     if not all(c in ALLOWED_CHARS for c in botname):
         error('Bot name must only contain alphanumeric characters or the underscore.')
         exit(1)
+
+    if not os.path.isfile(binary):
+        error('`{}` is not a file.'.format(binary))
+        exit(1)
+    
+    if srcpath is not None and not os.path.isdir(srcpath):
+        error('`{}` is not a directory.'.format(srcpath))
+        exit(1)
+    
+    check_player_in_db(botname)
 
     if srcpath is None:
         info('No source directory has been provided. '
@@ -152,22 +203,18 @@ def main(binary, srcpath, botname, botdesc):
             info('User canceled upload operation.')
             exit(1)
         
+    zip_path = None
     if srcpath is not None:
         zip_path = zip_source_code(srcpath)
         info('Zipped source code at `{}` to `{}`.'.format(srcpath, zip_path))
-        # upload_src_to_custom_s3_bucket(botname, binary)
-
-    quit()
 
     athena_user, iam_user = get_user()
     official_name = '{}.{}'.format(athena_user, botname)
 
     info('Bot official_name is `{}`.'.format(official_name))
 
-    # Fetch the bucket.
-    
     # Upload binary to custom bucket
-    upload_binary_to_custom_s3_bucket(botname, binary, iam_user)
+    upload_binary_to_custom_s3_bucket(botname, binary, zip_path, CUSTOM_S3_BUCKET)
 
     # Upload binary to 6.172 S3 bucket
     #test_6172_s3_bucket_valid()
