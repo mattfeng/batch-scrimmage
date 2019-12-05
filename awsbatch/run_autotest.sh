d36badf1137648e1cc192a1b8a36bda5aeb3ad31
Matthew Feng <mattfeng@mit.edu>
2019-12-05 15:11:48 +0000

    Zip up logged games

--- awsbatch/run_autotest.sh
+++ awsbatch/run_autotest.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-echo $(which conda)
-conda init bash
-conda activate base
 pip install boto3
 pip install requests
 
 cd /leiserchess
-wget https://leiserchess.s3.amazonaws.com/leiserchess_autotester.zip
-unzip leiserchess_autotester.zip
+wget https://leiserchess.s3.amazonaws.com/leiserchess_autotester-log.zip
+unzip leiserchess_autotester-log.zip
 
-cd leiserchess_autotester
+cd leiserchess_autotester-log
 cd BayesElo
 make
 cd ../autotester
@@ -25,8 +22,15 @@ wget $TEST_SPEC_URL -O autotest.txt
 echo 'Running autotests with the following ATFile:'
 cat autotest.txt
 
-echo '\n'
+echo -e '\n'
 echo 'Downloading players...'
 /leiserchess/download_players.py autotest.txt
 
 java -jar lauto.jar autotest.txt
+
+# get scrimmage UUID
+SCRIM_UUID=$2
+
+zip games_${SCRIM_UUID}.zip *.log autotest.pgn autotest.txt
+
+ls
